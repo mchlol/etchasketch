dd7deac1540b2a1b5648223cdd7b859520e46fa7
mchlol <m.andreabr@gmail.com>
2021-07-25 10:44:32 +1000

    grid, input, & add cell function created

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-const grid = document.querySelector('#grid');
+/* 
+create a grid container
+create an input element
+create a function to add a cell to the grid
+create a function to add n cells to the grid based on input e.g. 16 rows & 16 columns
+create a function to change the class of cells when hovered
+create a function to clear the cells 
+*/
 
-let div = document.createElement('div');
-div.classList.add('square');
+const grid = document.querySelector('#grid-container');
 
 
-grid.appendChild(div);
+// add a single cell to the grid
+
+function addCell() {
+const cell = document.createElement('div');
+cell.classList.add('cell');
+cell.textContent = "";
+grid.appendChild(cell);
+};
+
+addCell();
+addCell();
+
+// add a certain amount of cells x cells depending on user input
+
+const gridSize = document.querySelector("#grid-size");
+
+const grid16x16 = gridSize.elements['16x16'];
+let sizeChosen = grid16x16.value;
+
+gridSize.addEventListener('submit', (e) => {
+    // if grid chosen is 12x12 add 144 cells 12 across 12 down
+    gridSize.elements['16x16']
+});
+
+/* if (grid size chosen is 16 x 16) {
+    add cells
+
+*/
