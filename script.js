3c8b29313a4233b16bd98800a3ad097fe832f8bd
mchlol <m.andreabr@gmail.com>
2021-07-27 19:25:35 +1000

    changed input to prompt

--- script.js
+++ script.js
@@ -25,68 +25,25 @@ function addCells(value) {
     };
 };
 
-// call function with value
+// inital grid on load
 addCells(16*16);
 
-// clear grid
-const clearBtn = document.querySelector("#clear-grid");
-
-clearBtn.addEventListener('click', () => {
-    let cell = document.querySelector(".cell");
-    cell.style.backgroundColor = "white";
-});
-
-// change size
-function gridSizeFunc(num) {
-    if (num = 16) {
-        addCells(16*16);
-    } else if (num = 32) {
-        addCells(32*32);
-    } else if (num = 64) {
-        addCells(64*64);
-    } else {
-        return;
-    }
-};
-
-
 /*
-clear cells & prompt user for grid size
-  limit user input to 100 max
-  run function on button click
-  input from prompt to change size
-  container to remain at same size 
-  */
+click clear button
+prompt user for size eg 16
+grid loads with 16 x 16 cells
+*/
 
-// add a certain amount of cells x cells depending on user input - change css class?
-// set grid-template-rows / columns with a ${variable}
+const clearBtn = document.querySelector('#clear-button');
 
-// access form by id
-const form = document.querySelector("#grid-selector");
-
-// access value of selected radio button
-const grid16 = gridSize.elements['16x16'].value;
-const grid32 = gridSize.elements['32x32'].value;
-const grid64 = gridSize.elements['64x64'].value;
-
-// function to change grid based on input
-
-gridSize.addEventListener('submit', (e) => {
-    removeCells();
-    if (sizeChosen = grid16) { //sizeChosen has not been declared
-        addCells(16*16);
-    } else if (sizeChosen = grid32) {
-        addCells(32*32);
-    } else if (sizeChosen = grid64) {
-        addCells(64*64);
-    } else {
-        return;
-    }
-});
-
-// remove all child nodes
-function removeCells() {
+clearBtn.addEventListener("click", function () {
+    // remove all the cells
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
-};
+    // prompt input for cells to be added
+    let newGrid = prompt("Enter a new grid size");
+
+    // call addCells with value
+    addCells(newGrid);
+});
